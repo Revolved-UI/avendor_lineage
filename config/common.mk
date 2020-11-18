a1753cfca912da3c74e0f70ec5b6adf9adb7f239
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-18 19:01:11 -0300

    vendor: Import bootanimation from pixelstyle
    Change-Id: I54dfc3cf5c27a4aa10dac62892329cd25af99228

--- config/common.mk
+++ config/common.mk
@@ -105,10 +105,9 @@ $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUI
 endif
 
 # Bootanimation
+include vendor/lineage/config/bootanimation.mk
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
 
 # Build Manifest
 PRODUCT_PACKAGES += \
